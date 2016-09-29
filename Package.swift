d2400a6cc5f099dfdd6259a60dae222ee70c7d17
Ankur Patel <apatel471@bloomberg.net>
2016-09-29 14:16:53 -0400

    Using non beta version

--- Package.swift
+++ Package.swift
@@ -5,5 +5,5 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/ankurp/Cent.git", Version(6, 0, 0, prereleaseIdentifiers: ["beta"]))
+        .Package(url: "https://github.com/ankurp/Cent.git", Version(6, 0, 1))
     ])
